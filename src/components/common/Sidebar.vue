e6b68469fcfa840483265c0e6946b992d54643b4
HGahoo <50950762+HGahoo@users.noreply.github.com>
2019-12-01 18:10:16 +0800

    2019年12月1日 18:10:06

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -20,6 +20,43 @@
                     </el-menu-item>
                 </template>
             </template>
+            <div class="Other-btn">
+                 <!-- 消息中心 -->
+                <div class="btn-bell">
+                    <el-tooltip effect="dark" :content="message?`有${message}条未读消息`:`消息中心`" placement="bottom">
+                        <router-link to="/tabs">
+                            <i class="el-icon-bell"></i>
+                        </router-link>
+                    </el-tooltip>
+                    <span class="btn-bell-badge" v-if="message"></span>
+                </div>
+                <!-- 全屏显示 -->
+                <div class="btn-fullscreen" @click="handleFullScreen">
+                    <el-tooltip effect="dark" :content="fullscreen?`取消全屏`:`全屏`" placement="bottom">
+                        <i class="el-icon-rank"></i>
+                    </el-tooltip>
+                </div>
+                <!-- 用户头像 -->
+                <div class="user-avator">
+                    <img src="static/img/img.jpg">
+                    <el-dropdown class="user-name" trigger="click" @command="handleCommand" v-if="collapse==false">
+                        <span class="el-dropdown-link">
+                            {{username}} <i class="el-icon-caret-bottom"></i>
+                        </span>
+                        <el-dropdown-menu slot="dropdown">
+                            <!-- <a href="http://blog.gdfengshuo.com/about/" target="_blank">
+                                <el-dropdown-item>关于作者</el-dropdown-item>
+                            </a> -->
+                            <!-- <a href="https://github.com/lin-xin/vue-manage-system" target="_blank"> -->
+                                <!-- <el-dropdown-item>
+                                    <router-link to = "/personal_settings">个人设置</router-link>
+                                </el-dropdown-item> -->
+                            <!-- </a> -->
+                            <el-dropdown-item divided  command="loginout">退出登录</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                </div>
+            </div>
         </el-menu>
     </div>
 </template>
@@ -31,12 +68,19 @@
         data() {
             return {
                 collapse: false,
-                items: []
+                items: [],
+                name: 'jinma',
+                fullscreen: false,
+                message: 0,
             }
         },
         computed:{
             onRoutes(){
                 return this.$route.path.replace('/','');
+            },
+            username(){
+                let username = localStorage.getItem('ms_username');
+                return username ? username : this.name;
             }
         },
         created(){
@@ -48,6 +92,40 @@
             this.getDataInfo();
         },
         methods:{
+            // 用户名下拉菜单选择事件
+            handleCommand(command) {
+                if(command == 'loginout'){
+                    localStorage.removeItem('ms_username')
+                    this.$router.push('/login');
+                }
+            },
+            // 全屏事件
+            handleFullScreen(){
+                let element = document.documentElement;
+                if (this.fullscreen) {
+                    if (document.exitFullscreen) {
+                        document.exitFullscreen();
+                    } else if (document.webkitCancelFullScreen) {
+                        document.webkitCancelFullScreen();
+                    } else if (document.mozCancelFullScreen) {
+                        document.mozCancelFullScreen();
+                    } else if (document.msExitFullscreen) {
+                        document.msExitFullscreen();
+                    }
+                } else {
+                    if (element.requestFullscreen) {
+                        element.requestFullscreen();
+                    } else if (element.webkitRequestFullScreen) {
+                        element.webkitRequestFullScreen();
+                    } else if (element.mozRequestFullScreen) {
+                        element.mozRequestFullScreen();
+                    } else if (element.msRequestFullscreen) {
+                        // IE11
+                        element.msRequestFullscreen();
+                    }
+                }
+                this.fullscreen = !this.fullscreen;
+            },
             getDataInfo(){
                 var storage=window.localStorage
                 var account=storage.ms_username
@@ -279,7 +357,7 @@
         height: 100%;
         top: 60px;
         bottom:0;
-         overflow-y: scroll;  /*裁剪内容 - 提供滚动机制。 */
+        overflow-y: scroll;  /*裁剪内容 - 提供滚动机制。 */
     }
     .sidebar::-webkit-scrollbar{
         width: 0;
@@ -289,5 +367,59 @@
     }
     .sidebar > ul {
         height:100%;
+    }
+    .user-avator{
+        margin-top: 10px;
+        margin-left: 12px;
+    }
+    .user-avator img{
+        display: inline-block;
+        width:40px;
+        height:40px;
+        border-radius: 50%;
+    }
+    .user-name{
+        margin-left: 10px;
+        display: inline-block;
+        position: relative;
+        top: -15px
+    }
+    .el-dropdown-link{
+        color: #fff;
+        cursor: pointer;
+    }
+    .btn-fullscreen{
+        display: inline-block;
+        transform: rotate(45deg);
+        /* margin-right: 20px; */
+        color: #fff;
+    }
+    .btn-bell, .btn-fullscreen{
+        margin-left: 15px;
+        font-size: 25px;
+        display: inline-block;
+        position: relative;
+        width: 30px;
+        height: 30px;
+        text-align: center;
+        border-radius: 15px;
+        cursor: pointer;
+    }
+    .btn-bell-badge{
+        position: absolute;
+        right: 0;
+        top: -2px;
+        width: 8px;
+        height: 8px;
+        border-radius: 4px;
+        background: #f56c6c;
+        color: #fff;
+    }
+    .btn-bell .el-icon-bell{
+        color: #fff;
+    }
+    .Other-btn{
+        position: fixed;
+        bottom: 0px;
     }
 </style>
